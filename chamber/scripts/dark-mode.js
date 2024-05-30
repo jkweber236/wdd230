b85b052d5daeb1076be00144af0d5874e63bc267
Jennifer Weber <jkweber236@gmail.com>
2024-05-30 01:33:24 -0600

    Changed hero image and color scheme

--- chamber/scripts/dark-mode.js
+++ chamber/scripts/dark-mode.js
@@ -1,6 +1,10 @@
 const modeButton = document.querySelector(".switch input");
 const main = document.querySelector("main");
+const header = document.querySelector("header");
+const footer = document.querySelector("footer")
 
 modeButton.addEventListener("change", () => {
     main.classList.toggle('dark-mode');
+    header.classList.toggle('dark-mode');
+    footer.classList.toggle('dark-mode');
 });
