693bf28ee9cde454ebaf9319725f33d3291417bd
Jennifer Weber <jkweber236@gmail.com>
2024-06-06 15:20:36 -0600

    Completed lazy load activity

---
+++ lesson07/scripts/bom.js
@@ -0,0 +1,26 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list')
+
+let chaptersArray = getChapterList() || [];
+chaptersArray.forEach(chapter => {
+    displayList(chapter);
+});
+
+button.addEventListener('click', () => {
+    if (input.value != '' && list.children.length < 10) {
+        const li = document.createElement('li');
+        const deletebutton = document.createElement('button');
+        li.textContent = input.value;
+        deletebutton.textContent = '❌';
+        li.append(deletebutton);
+        list.append(li);
+
+        deletebutton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        })
+        input.value = '';
+    }
+    input.focus();
+});
